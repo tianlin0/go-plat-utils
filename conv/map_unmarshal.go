0d1eeb957b51d66189e1a4a6c5790d74f4521aef
tianlin0 <tianlin0@qq.com>
2025-03-03 15:04:42 +0800

    fix: 修改小bug

--- conv/map_unmarshal.go
+++ conv/map_unmarshal.go
@@ -13,9 +13,15 @@ var Marshal = String
 // Unmarshal 将前一个的对象填充到后一个对象中，字段名相同的覆盖值，
 // 返回 interface 的作用是如果toPoint为nil的时候，也能正常返回对象.
 func Unmarshal(srcStruct interface{}, dstPoint interface{}) error {
-	srcValTemp := reflect.ValueOf(srcStruct)
-	if srcStruct == nil || srcValTemp.IsNil() {
+	if srcStruct == nil {
 		return nil
+	}
+	srcType := reflect.TypeOf(srcStruct)
+	srcVal := reflect.ValueOf(srcStruct)
+	if srcType.Kind() == reflect.Ptr {
+		if srcVal.IsNil() {
+			return nil
+		}
 	}
 
 	isString := false
@@ -36,7 +42,6 @@ func Unmarshal(srcStruct interface{}, dstPoint interface{}) error {
 	}
 
 	// 1、首先看能否直接赋值
-	srcType := reflect.TypeOf(srcStruct)
 	dstType := reflect.TypeOf(dstPoint)
 	if srcType == dstType {
 		if srcType.Kind() != reflect.Ptr &&
